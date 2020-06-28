4ec2aa71b98cf97c1b0d87db3128a8fe4102c173
Cris-ti-na <cristina.ccmm@gmail.com>
2020-06-28 13:54:22 +0200

    add hidden social media icons function

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,20 @@
-"use strict";
+'use strict';
 
 // Elemento de donde cogemos los datos
-const nameElement = document.querySelector(".js-input-name");
-const positionElement = document.querySelector(".js-input-position");
-const emailElement = document.querySelector(".js-input-email");
-const telephoneElement = document.querySelector(".js-input-telephone");
-const linkedinElement = document.querySelector(".js-input-linkedin");
-const githubElement = document.querySelector(".js-input-github");
+const nameElement = document.querySelector('.js-input-name');
+const positionElement = document.querySelector('.js-input-position');
+const emailElement = document.querySelector('.js-input-email');
+const telephoneElement = document.querySelector('.js-input-telephone');
+const linkedinElement = document.querySelector('.js-input-linkedin');
+const githubElement = document.querySelector('.js-input-github');
 
 // Elemento donde pintamos los datos
-const name = document.querySelector(".js-card-name");
-const position = document.querySelector(".js-card-position");
-const email = document.querySelector(".js-icon-email");
-const telephone = document.querySelector(".js-icon-telephone");
-const linkedin = document.querySelector(".js-icon-linkedin");
-const github = document.querySelector(".js-icon-github");
+const name = document.querySelector('.js-card-name');
+const position = document.querySelector('.js-card-position');
+/*const email = document.querySelector('.js-icon-email');
+const telephone = document.querySelector('.js-icon-telephone');
+const linkedin = document.querySelector('.js-icon-linkedin');
+const github = document.querySelector('.js-icon-github');*/
 
 // recogemos datos de los inputs
 const userData = {};
@@ -40,25 +40,25 @@ function result() {
 
 // Pintamos (cogemos los datos del input)
 
-nameElement.addEventListener("keyup", result);
-positionElement.addEventListener("keyup", result);
-emailElement.addEventListener("keyup", result);
-telephoneElement.addEventListener("keyup", result);
-linkedinElement.addEventListener("keyup", result);
-githubElement.addEventListener("keyup", result);
+nameElement.addEventListener('keyup', result);
+positionElement.addEventListener('keyup', result);
+emailElement.addEventListener('keyup', result);
+telephoneElement.addEventListener('keyup', result);
+linkedinElement.addEventListener('keyup', result);
+githubElement.addEventListener('keyup', result);
 
 //Mostrar iconos de contacto según se rellena el formulario
-const iconTelephone = document.querySelector(".js-telephone");
-const iconEmail = document.querySelector(".js-email");
-const iconLinkedin = document.querySelector(".js-linkedin");
-const iconGithub = document.querySelector(".js-github");
+const iconTelephone = document.querySelector('.js-telephone');
+const iconEmail = document.querySelector('.js-email');
+const iconLinkedin = document.querySelector('.js-linkedin');
+const iconGithub = document.querySelector('.js-github');
 
 function showContactTelephone() {
   if (telephoneElement.value.length !== 0) {
-    iconTelephone.classList.remove("hidden");
+    iconTelephone.classList.remove('hidden');
   } else {
-    iconTelephone.classList.add("hidden");
+    iconTelephone.classList.add('hidden');
   }
 }
 
-telephoneElement.addEventListener("keyup", showContactTelephone);
+telephoneElement.addEventListener('keyup', showContactTelephone);
