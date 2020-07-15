07789bdfe8873931436d0f6aeeb9b437a7154235
Junyi Ji <junyiji@gmail.com>
2020-07-15 20:20:39 +0200

    Add local storage function

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,29 @@
 "use strict";
 
+let userData = {
+name: '',
+job: '',
+email: '',
+phone: '',
+linkedin: '',
+github: '',
+photo: '',
+// userData.paletteCold = palette1Element.value;
+// userData.palette2 = palette2Element.value;
+// userData.palette3 = palette3Element.value;
+
+palette: '',
+}
+
+
+
 // Botón de subir imagen
 const fr = new FileReader();
 const uploadBtn = document.querySelector(".js__profile-trigger");
 const fileField = document.querySelector(".js__profile-upload-btn");
 let profileImage = document.querySelector(".js__profile-image");
 const profilePreview = document.querySelector(".js__profile-preview");
+
 
 /**
  * Recoge el archivo añadido al campo de tipo "file"
@@ -33,6 +51,9 @@ function writeImage() {
    */
   profileImage.style.backgroundImage = `url(${fr.result})`;
   profilePreview.style.backgroundImage = `url(${fr.result})`;
+
+  userData.photo = fr.result;
+  localStorage.setItem('photo', userData.photo);
 }
 
 /**
@@ -58,9 +79,11 @@ const emailElement = document.querySelector(".js-input-email");
 const phoneElement = document.querySelector(".js-input-telephone");
 const linkedinElement = document.querySelector(".js-input-linkedin");
 const githubElement = document.querySelector(".js-input-github");
-const palette1Element = document.querySelector("#palette-1");
-const palette2Element = document.querySelector("#palette-2");
-const palette3Element = document.querySelector("#palette-3");
+// const palette1Element = document.querySelector("#palette-1");
+// const palette2Element = document.querySelector("#palette-2");
+// const palette3Element = document.querySelector("#palette-3");
+
+
 
 // Elemento donde pintamos los datos
 const name = document.querySelector(".js-card-name");
@@ -72,8 +95,14 @@ const github = document.querySelector(".js-icon-github");
 const photo = document.querySelector(".js__profile-image");
 const responseURL = document.querySelector(".result-url");
 
+
+
 // Paletas
-const inputsList = document.querySelectorAll(".js-palette");
+// const inputsList = document.querySelectorAll(".js-palette");
+// const palette = inputsList;
+const palette = document.querySelectorAll(".js-palette");
+
+
 const bar = document.querySelector(".card__id");
 const iconSm = document.querySelectorAll(".contact__icon");
 const photoDefault = document.querySelector(".card__photo");
@@ -82,14 +111,16 @@ const paletteWarm = document.querySelector("#palette-2");
 const paletteNeutral = document.querySelector("#palette-3");
 
 function addInputsListeners() {
-  for (const input of inputsList) {
+  for (const input of palette) {
     input.addEventListener("change", addPaletteClass);
   }
 }
 
 function addPaletteClass(evt) {
-  const inputValue = evt.currentTarget.value;
-  console.log(inputValue);
+  // const inputValue = evt.currentTarget.value;
+  // const palette = inputValue
+ const paletteValue = evt.currentTarget.value;
+  console.log(paletteValue);
   name.classList.remove("card__id_name");
   name.classList.remove("card__id_name2");
   name.classList.remove("card__id_name3");
@@ -105,7 +136,7 @@ function addPaletteClass(evt) {
     icon.classList.remove("contact__icon3");
   }
 
-  if (inputValue === "coldcolors") {
+  if (paletteValue === "1") {
     name.classList.add("card__id_name");
     bar.classList.add("card__id");
     photoDefault.classList.add("card__photo");
@@ -113,7 +144,7 @@ function addPaletteClass(evt) {
     for (const icon of iconSm) {
       icon.classList.add("contact__icon");
     }
-  } else if (inputValue === "warmcolors") {
+  } else if (paletteValue === "2") {
     name.classList.add("card__id_name2");
     bar.classList.add("card__id2");
     photoDefault.classList.add("card__photo2");
@@ -128,6 +159,9 @@ function addPaletteClass(evt) {
       icon.classList.add("contact__icon3");
     }
   }
+  // for (let input of palette){
+  //   input.addEventListener("change", result);
+  // }
 }
 
 addInputsListeners();
@@ -136,7 +170,7 @@ addInputsListeners();
 
 // ---- OBJETO (REVISAR)
 // recogemos datos de los inputs
-const userData = {};
+userData = {};
 function getInputValues() {
   userData.name = nameElement.value;
   userData.job = positionElement.value;
@@ -145,12 +179,81 @@ function getInputValues() {
   userData.linkedin = linkedinElement.value;
   userData.github = githubElement.value;
   userData.photo = fr.result;
-  userData.paletteCold = palette1Element.value;
-  userData.palette2 = palette2Element.value;
-  userData.palette3 = palette3Element.value;
+  // userData.paletteCold = palette1Element.value;
+  // userData.palette2 = palette2Element.value;
+  // userData.palette3 = palette3Element.value;
+  
+ for (let item of palette){
+  userData.palette = item.value;
+ }
+
+  // userData.name.classList.add("card__id_name");
+  //   bar.classList.add("card__id");
+  //   photoDefault.classList.add("card__photo");
+  // CAMBIAR PALETTE
+
+  setInLocalStorage();
+
+  // function localStorage(data){
+  //   userData.name.value = data.name
+
+  //   }
+  
 }
 
+function setInLocalStorage() {
+  localStorage.setItem('data', JSON.stringify(userData));
+}
+
+
+const localImage = localStorage.getItem('photo');
+if (localImage) {
+  profileImage.style.backgroundImage = `url(${localImage})`;
+  profilePreview.style.backgroundImage = `url(${localImage})`;
+  userData.photo = localImage;
+}
+
+
+// function getFromLocalStorage(){
+//   const dataFromLS = JSON.parse(localStorage.getItem('data'))
+//   if (dataFromLS !== null){
+//     userData = dataFromLS
+//   } 
+// } 
+
+
+function getFromLocalStorage() {
+  // return JSON.parse(localStorage.getItem('data')) || [] ;
+  // userData.name = JSON.parse(localStorage.getItem('data'));
+  // const dataFromLS = JSON.parse(localStorage.getItem('data'));
+  // return dataFromLS === null ? [] : dataFromLS;
+  const savedInfo = JSON.parse(localStorage.getItem('data'));
+  function uploadInfo() {
+    if (savedInfo !== null) {
+    nameElement.value = savedInfo.name || '';
+    positionElement.value = savedInfo.job || '';
+    // AQUIIII
+    emailElement.value = savedInfo.email || '';
+    phoneElement.value = savedInfo.phone || '';
+    linkedinElement.value = savedInfo.linkedin || '';
+    githubElement.value = savedInfo.github || '';
+
+
+    // profilePreview.style.backgroundImage =  `url(${dataUser.photo})`|| `url(./assets/images/p1.jpg)`;
+    // profileImage.style.backgroundImage =  `url(${dataUser.photo})` || `url(./assets/images/p1.jpg)`;
+    }
+
+    
+  
+  }
+  uploadInfo();
+}
+
+
+
 // Elemento de donde pintamos los datos
+
+
 function updateCard(obj) {
   name.innerHTML = obj.name || "Nombre Apellido";
   job.innerHTML = obj.job || "Puesto de trabajo";
@@ -158,6 +261,9 @@ function updateCard(obj) {
   email.setAttribute("href", "mailto:" + obj.email);
   linkedin.setAttribute("href", obj.linkedin);
   github.setAttribute("href", "https://github.com/", obj.github);
+  // for(let item of palette){
+  //   item.setAttribute("value", obj.palette)
+  // }
 }
 console.log(email);
 
@@ -165,6 +271,9 @@ function result() {
   getInputValues();
   updateCard(userData);
 }
+
+
+
 
 // Pintamos (cogemos los datos del input)
 
@@ -174,9 +283,10 @@ emailElement.addEventListener("keyup", result);
 phoneElement.addEventListener("keyup", result);
 linkedinElement.addEventListener("keyup", result);
 githubElement.addEventListener("keyup", result);
-paletteCold.addEventListener("keyup", result);
-paletteWarm.addEventListener("keyup", result);
-paletteNeutral.addEventListener("keyup", result);
+// paletteCold.addEventListener("keyup", result);
+// paletteWarm.addEventListener("keyup", result);
+// paletteNeutral.addEventListener("keyup", result);
+
 
 //Mostrar iconos de contacto según se rellena el formulario
 const iconTelephone = document.querySelector(".js-telephone");
@@ -306,7 +416,10 @@ function sendRequest(objectData) {
     .catch(function (error) {
       console.log(error);
     });
+
 }
+
+
 
 const twitterContainer = document.querySelector(".js-twitter");
 const twitterButton = document.querySelector(".js-twitter-button");
@@ -325,6 +438,10 @@ function showURL(result) {
     responseURL.innerHTML = "ERROR:" + result.error;
   }
 }
+
+getFromLocalStorage()
+
+
 
 /*
 function addEmail () {
